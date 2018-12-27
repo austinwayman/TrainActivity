c74c292c970e36f04a9252edc8f7fe27913992da
Austin Wayman <awguitar@msn.com>
2018-12-27 16:33:50 -0700

    readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # TrainActivity
 
-I couldnt figure out how to get the times to list in order unfortunately but I'm pretty proud of what I have! Thanks!
+This is a train tracker where you can add a train's name, destination, the first departure of the day, then the frequency. 
+
+Then, using javascript, the site will tell you when the next train will arrive. 
--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-// Steps to complete:
-
-// 1. Initialize Firebase
-// 2. Create button for adding new employees - then update the html + update the database
-// 3. Create a way to retrieve employees from the employee database.
-// 4. Create a way to calculate the months worked. Using difference between start and current time.
-//    Then use moment.js formatting to set difference in months.
-// 5. Calculate Total billed
-
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyCDC92McgpwzTxtrKLBMxuLlJmhRkB89ds",
@@ -112,10 +103,3 @@ database.ref().on("child_added", function (childSnapshot) {
     $("#train-table > tbody").append(newRow);
 });
 
-  // Example Time Math
-  // -----------------------------------------------------------------------------
-  // Assume Employee start date of January 1, 2015
-  // Assume current date is March 1, 2016
-
-  // We know that this is 15 months.
-  // Now we will create code in moment.js to confirm that any attempt we use meets this test case
